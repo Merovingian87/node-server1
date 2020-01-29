6957edf7ca5f8cdba66fa2ab5422c994144f5197
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2020-01-29 10:57:27 -0600

    End of "Hello World" server

---
+++ index.js
@@ -0,0 +1,15 @@
+// My first server
+
+const http = require('http');
+const hostName = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World\n');
+});
+
+server.listen(port, hostName, () => {
+  console.log(`server running at http://${hostName}:${port}/`);
+});
